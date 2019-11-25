68b0b0cf37d3be2979db083eb0593c0aef7aa108
Carlos Garcia <carlos.garcia@gigigo.com.mx>
2019-11-25 11:03:06 -0600

    Se agregan imagenes de dados y botón tirar

--- 02-dices/02-dices/ViewController.swift
+++ 02-dices/02-dices/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageViewDiceLeft: UIImageView!
+    @IBOutlet weak var imageViewDiceRight: UIImageView!
+    
+    var randomDiceIndexLeft : Int = 0
+    var randomDiceIndexRight : Int = 0
+    let diceImages = ["dado1", "dado2", "dado3", "dado4", "dado5", "dado6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        generateRandomDices()
     }
 
-
+    @IBAction func rollPressed(_ sender: UIButton) {
+        generateRandomDices()
+    }
+    
+    func generateRandomDices() {
+        
+    }
+    
 }
 
