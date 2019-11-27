8976b2e032e3c056f3631cce44f1799f5b632110
Carlos Garcia <carlos.garcia@gigigo.com.mx>
2019-11-27 15:18:18 -0600

    Se agrega animación

--- 02-dices/02-dices/ViewController.swift
+++ 02-dices/02-dices/ViewController.swift
@@ -14,7 +14,14 @@ class ViewController: UIViewController {
     
     var randomDiceIndexLeft : Int = 0
     var randomDiceIndexRight : Int = 0
-    let diceImages = ["dado1", "dado2", "dado3", "dado4", "dado5", "dado6"]
+    let diceImages: [String]
+    let nFaces : UInt32
+    
+    required init?(coder aDecoder: NSCoder) {
+        diceImages = ["dado1", "dado2", "dado3", "dado4", "dado5", "dado6"]
+        nFaces = UInt32(diceImages.count)
+        super.init(coder: aDecoder)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +35,37 @@ class ViewController: UIViewController {
     
     func generateRandomDices() {
         
+        //Generar aleatoriamente y cambiar el dado izquierdo
+        randomDiceIndexLeft = Int(arc4random_uniform(nFaces))
+        let nameImageDiceLeft = diceImages[randomDiceIndexLeft]
+        imageViewDiceLeft.image = UIImage(named: nameImageDiceLeft)
+        
+        //Generar aleatoriamente y cambiar el dado derecho
+        randomDiceIndexRight = Int(arc4random_uniform(nFaces))
+        let nameImageDiceRight = diceImages[randomDiceIndexRight]
+        imageViewDiceRight.image = UIImage(named: nameImageDiceRight)
+        
+        UIView.animate(withDuration: 0.3,
+                       delay: 0,
+                       options: UIView.AnimationOptions.curveEaseOut,
+                       animations: {
+                        self.imageViewDiceLeft.transform = CGAffineTransform(scaleX: 0.6, y: 0.6).concatenating(CGAffineTransform(rotationAngle: CGFloat.pi/2)).concatenating(CGAffineTransform(translationX: -100, y: 100))
+                        
+                        self.imageViewDiceRight.transform = CGAffineTransform(scaleX: 0.6, y: 0.6).concatenating(CGAffineTransform(rotationAngle: CGFloat.pi/2)).concatenating(CGAffineTransform(translationX: 100, y: 100))
+                        
+                        self.imageViewDiceRight.alpha = 0.0
+                        self.imageViewDiceLeft.alpha = 0.0
+        }) { (completed) in
+            self.imageViewDiceLeft.transform = CGAffineTransform.identity
+            self.imageViewDiceRight.transform = CGAffineTransform.identity
+            
+            self.imageViewDiceRight.alpha = 1.0
+            self.imageViewDiceLeft.alpha = 1.0
+            
+            self.imageViewDiceLeft.image = UIImage(named: nameImageDiceLeft)
+            self.imageViewDiceRight.image = UIImage(named: nameImageDiceRight)
+        }
+
     }
     
 }
